515cc185bd47529dfcd6661044cdba1c6fe7b4f3
YarinBS <yarin10121@gmail.com>
2025-07-19 17:13:23 +0300

    Set default number of output tokens to 0

--- utils/token_tracker.py
+++ utils/token_tracker.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 
-def log_token_usage(operation: str, input_tokens: int, output_tokens: int) -> None:
+def log_token_usage(operation: str, input_tokens: int, output_tokens: int = 0) -> None:
     """Logs the token usage to the total_tokens.txt file"""
 
     if operation not in ["chat", "embedding"]:
