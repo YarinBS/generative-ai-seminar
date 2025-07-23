f402835accf9db514944de3480f78ff0cd2e6ccd
YarinBS <yarin10121@gmail.com>
2025-07-23 17:58:16 +0300

    Add Alexupport skeleton

--- agent/alexupport_agent.py
+++ agent/alexupport_agent.py
@@ -1,4 +1,47 @@
 """
 Main Alexupport agent
-This module contains the main agent which is built using the other microagents
+This module contains the main agent which orchestrates the other microagents
 """
+
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import HumanMessage, AIMessage
+
+from agent.answer_generator import AnswerGenerator
+from agent.followup_generator import FollowUpGenerator
+from agent.input_refiner import InputRefiner
+from agent.is_answerable_agent import IsAnswerableAgent
+from agent.is_relevant_agent import IsRelevantAgent
+from agent.llm_client import LLMClient
+
+LLM_CLIENT = LLMClient()
+
+class AlexupportAgent:
+    """Main Alexupport agent"""
+
+    def __init__(self):
+
+        self.introduction = """
+        ADD INSTRUCTIONS HERE
+        """
+
+        self.memory = ConversationBufferMemory()
+
+        # Initializing all microagents
+        self.input_refiner = InputRefiner(llm_client=LLM_CLIENT, chat_history=self.memory)
+        self.is_answerable_agent = IsAnswerableAgent(llm_client=LLM_CLIENT)
+        self.answer_generator = AnswerGenerator(llm_client=LLM_CLIENT)
+        self.followup_generator = FollowUpGenerator(llm_client=LLM_CLIENT)
+        self.is_relevant_generator = IsRelevantAgent(llm_client=LLM_CLIENT)
+
+    def get_agent_introduction(self) -> str:
+        """Displays the agent introduction and instructions to the user upon activation"""
+        return self.introduction
+
+    def answer_user_query(self, user_query: str) -> str:
+        """Main Alexupport pipeline"""
+        # Step 0 - Clear previous chat history, if exists
+        self.memory.clear()
+
+        #TODO: Implement the complete pipeline
+
+        raise NotImplementedError()
