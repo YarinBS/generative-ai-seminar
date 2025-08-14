ba07716b98f95a1a6f4cb20a9dc97d810b8c2d61
YarinBS <yarin10121@gmail.com>
2025-08-14 18:45:50 +0300

    Initialize agent and display introduction in the UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ from agent.alexupport_agent import AlexupportAgent
 import random
 import time
 
+agent = AlexupportAgent()
+
 QDRANT_URL = "https://63ad19dc-7779-4868-bc81-41f5fae4353a.europe-west3-0.gcp.cloud.qdrant.io:6333"
 QDRANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0._mnughDNgXpg2I_tMDwpIIKZJiDqma2o_YDld0ZseR4"
 COLLECTION_NAME = "data_collection"
@@ -19,6 +21,7 @@ asin_title_map = {}
 asin_list = sorted({point.payload['asin'] for point in points})
 
 st.title("Alexupport - Amazon AI Support Agent 🤖")
+st.text(agent.get_agent_introduction())
 
 # Step 1: User selects an ASIN (starts empty)
 asin_options = ["Select an ASIN..."] + asin_list
@@ -33,18 +36,18 @@ asin_list = sorted(asin_title_map.keys())
 product = next((point for point in points if point.payload.get('asin') == selected_asin), None)
 product_title = product.payload['productTitle'] if product else 'No title found'
 
-st.success(f"You selected ASIN: {selected_asin}")
-st.info(f"**Product Title:** {product_title}")
+# st.success(f"You selected ASIN: {selected_asin}")
+st.info(f"**Product Title:** {product_title}\n\nYou can view the product page here: \nhttps://www.amazon.com/dp/{selected_asin}")
 st.markdown("---")
-product_title = asin_title_map.get(selected_asin, 'No title found')
-
-
-
+# product_title = asin_title_map.get(selected_asin, 'No title found')
 
 # Initialize chat history and reset if ASIN changes
 if "asin" not in st.session_state or st.session_state["asin"] != selected_asin:
     st.session_state["asin"] = selected_asin
     st.session_state["messages"] = []
+    # Start chat with AlexupportAgent's chat start message
+    chat_start_msg = agent.get_agent_chat_start(product_title)
+    st.session_state["messages"].append({"role": "assistant", "content": chat_start_msg})
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -79,6 +82,8 @@ if prompt := st.chat_input("Your question here..."):
                 time.sleep(0.05)
 
         response = st.write_stream(response_generator(asin=selected_asin))
+
+        # response = st.write_stream(agent.answer_user_query(user_query=prompt))
         # ----- Agent response logic -----
 
     # Add assistant response to chat history
